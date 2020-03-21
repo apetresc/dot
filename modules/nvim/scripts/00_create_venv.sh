98111ecd123bb2f6abe718367861ed6dd2db1247
Adrian Petrescu <apetresc@gmail.com>
2020-03-20 21:43:01 -0400

    nvim: also generate python2-based pynvim venv

--- modules/nvim/scripts/00_create_venv.sh
+++ modules/nvim/scripts/00_create_venv.sh
@@ -4,3 +4,10 @@ if [[ ! -d ~/.virtualenvs/py3nvim ]]; then
 	virtualenv -p $(which python3) ~/.virtualenvs/py3nvim
 	~/.virtualenvs/py3nvim/bin/pip install pynvim
 fi
+
+if [[ ! -d ~/.virtualenvs/pynvim ]]; then
+	virtualenv -p $(which python2) ~/.virtualenvs/pynvim
+	~/.virtualenvs/pynvim/bin/pip install pynvim
+fi
+
+
