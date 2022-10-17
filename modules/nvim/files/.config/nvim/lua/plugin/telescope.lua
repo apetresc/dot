0e2fc58971bcb65d33dabbaa1d70b46487034d3c
Adrian Petrescu <adrian@apetre.sc>
2022-10-17 07:07:35 -0400

    nvim: switch telescope keybindings

--- modules/nvim/files/.config/nvim/lua/plugin/telescope.lua
+++ modules/nvim/files/.config/nvim/lua/plugin/telescope.lua
@@ -4,16 +4,12 @@ end
 
 require('telescope').setup{
   defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
     mappings = {
       i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key",
+        ["<C-?>"] = "which_key",
         ["<C-j>"] = "move_selection_next",
         ["<C-k>"] = "move_selection_previous",
+        ["<C-h>"] = "select_horizontal",
       }
     }
   },
