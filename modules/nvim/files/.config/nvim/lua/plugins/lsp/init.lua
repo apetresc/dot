0a16e1c1531d5713617f62f26490b3630f32987b
Adrian Petrescu <adrian@apetre.sc>
2023-03-06 11:39:26 -0500

    nvim: clean up lsp keybindings

--- modules/nvim/files/.config/nvim/lua/plugins/lsp/init.lua
+++ modules/nvim/files/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,21 +1,24 @@
 function bind_common_keys(_, bufnr)
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', '<c-]>', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gh', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', 'gl', vim.lsp.buf.incoming_calls, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
-  -- vim.keymap.set('n', 'gs', "<Cmd>lua vim.lsp.buf.document_symbol()<CR>", bufopts)
-  vim.keymap.set('n', 'gs', "<Cmd>SymbolsOutline<CR>", bufopts)
-  vim.keymap.set('n', 'gw', vim.lsp.buf.workspace_symbol, bufopts)
-  vim.keymap.set('n', '[x', vim.diagnostic.goto_prev, bufopts)
-  vim.keymap.set('n', ']x', vim.diagnostic.goto_next, bufopts)
-  vim.keymap.set('n', ']r', vim.diagnostic.open_float, bufopts)
-  vim.keymap.set('n', ']s', vim.diagnostic.show, bufopts)
+  local default_bufopts = { noremap = true, silent = true, buffer = bufnr }
+  local function Map(mode, lhs, rhs, desc, bufopts)
+    return vim.keymap.set(mode, lhs, rhs, vim.tbl_extend('force', bufopts or default_bufopts, {desc = desc}))
+  end
+  Map('n', '<c-]>', vim.lsp.buf.definition, 'Go to definition')
+  Map('n', 'K', vim.lsp.buf.hover, 'Display hover information about the symbol under the cursor')
+  Map('n', 'gh', vim.lsp.buf.signature_help, 'Display signature help')
+  Map('n', 'ga', vim.lsp.buf.code_action, 'Show code actions')
+  Map('n', 'gi', vim.lsp.buf.implementation, 'Go to implementation')
+  Map('n', 'gl', vim.lsp.buf.incoming_calls, 'List incoming calls')
+  Map('n', 'gd', vim.lsp.buf.type_definition, 'Go to type definition')
+  Map('n', 'gr', vim.lsp.buf.type_definition, 'List references')
+  Map('n', '<leader>r', vim.lsp.buf.rename, 'Rename symbol')
+  Map('n', 'gs', ':SymbolsOutline<CR>', 'Symbols outline')
+  Map('n', 'gw', vim.lsp.buf.workspace_symbol, 'List workspace symbols')
+  Map('n', '[x', vim.diagnostic.goto_prev, 'Go to previous diagnostic')
+  Map('n', ']x', vim.diagnostic.goto_next, 'Go to next diagnostic')
+  Map('n', ']s', vim.diagnostic.show, 'Show diagnostic')
+  Map('n', ']s', vim.diagnostic.show, 'Show diagnostic')
+  Map('n', ']r', vim.diagnostic.open_float, 'Float diagnostic')
 
   local id = vim.api.nvim_create_augroup("SharedLspFormatting", { clear = true })
   vim.api.nvim_create_autocmd({ "BufWritePre" }, {
@@ -67,6 +70,11 @@ return {
         end
       })
     end
+  },
+  {
+    'simrat39/symbols-outline.nvim',
+    config = true,
+    cmd = 'SymbolsOutline'
   },
   {
     'simrat39/rust-tools.nvim',
