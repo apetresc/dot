deff42bbc3111ec3a96c45ef800faee07d6dc91b
Adrian Petrescu <adrian@apetre.sc>
2023-04-03 14:05:38 -0400

    nvim: fix bad indentation in cmp.lua

--- modules/nvim/files/.config/nvim/lua/plugins/cmp.lua
+++ modules/nvim/files/.config/nvim/lua/plugins/cmp.lua
@@ -21,11 +21,11 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<c-y>"] = cmp.mapping(
-        cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Insert,
-          select = true,
-        },
-        { "i", "c" }
+          cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          },
+          { "i", "c" }
         ),
 
         ["<c-space>"] = cmp.mapping {
