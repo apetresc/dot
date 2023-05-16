563d09024552680c52f66717164186bb0fe2560f
Adrian Petrescu <adrian@apetre.sc>
2023-05-16 14:57:55 -0400

    nvim: fix stupid formatting_sync deprecation

--- modules/nvim/files/.config/nvim/lua/plugins/lsp/init.lua
+++ modules/nvim/files/.config/nvim/lua/plugins/lsp/init.lua
@@ -24,7 +24,9 @@ function bind_common_keys(_, bufnr)
   vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     group = id,
     pattern = "*",
-    command = "lua vim.lsp.buf.formatting_sync()",
+    callback = function()
+      vim.lsp.buf.format({async = false})
+    end,
   })
 end
 
