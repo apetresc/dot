9d3e3727c2d36decad9cda3c83d057071816278d
Adrian Petrescu <adrian@apetre.sc>
2024-01-22 14:16:30 -0500

    nvim: get off nightly branch for nvim-tree

--- modules/nvim/files/.config/nvim/lua/plugins/init.lua
+++ modules/nvim/files/.config/nvim/lua/plugins/init.lua
@@ -94,7 +94,7 @@ return {
   {
     'nvim-tree/nvim-tree.lua',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    version = 'nightly', -- optional, updated every week. (see issue #1193)
+    version = '*',
     config = function()
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
