4f29af74389b6486d6bdef69685f9dbe58b21090
Adrian Petrescu <adrian@apetre.sc>
2024-04-08 10:34:18 -0400

    nvim: add CopilotChat.nvim

--- modules/nvim/files/.config/nvim/lua/plugins/copilot.lua
+++ modules/nvim/files/.config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,18 @@
 return {
   'github/copilot.vim',
+
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
+
